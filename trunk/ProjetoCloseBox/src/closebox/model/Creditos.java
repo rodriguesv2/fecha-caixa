c26af34328b1f2f0e9439d6386bafa02b10ca329
rubensrodriguesv2@gmail.com <rubensrodriguesv2@gmail.com@fd6cb31d-0258-5aa5-8458-2ef2d50dd589>
2012-11-06 02:45:19 +0000

    Acerto dos creditos.

--- trunk/ProjetoCloseBox/src/closebox/model/Creditos.java
+++ trunk/ProjetoCloseBox/src/closebox/model/Creditos.java
@@ -4,35 +4,44 @@ public class Creditos {
 	
 	public static String getCredito(){
 		
-		return "Metodologia utilizada:\n\n- SCRUM\n\nIntegrantes\n\n- Fernando Gomes\n- Fernnada Onishi\n- Reinaldo Fonseca\n- Rubens Rodrigues\n- Thiago Mascarelo\n\n" +
-		"Orientador\n\n- Professor Edson Saraiva\n\n" +
-		
-		"Ferramentas:\n\n"+
-		"- IDE Eclipse versão mínima Hélios 3.6 para Java Developers\n" +
-		"- Plugin Android Development Tools (ADT)\n" +
-		"- Java Plataform, Standard Edition Development Kit (JDK) versão mínima 6 update 31\n" +
-		"- Android SDK (Standard Development Kit)\n" +
-		"- Ferramentas de vídeo-conferência e programa que ofereça serviço de Instant Messengers\n" +
-		"- MS Office 2010\n" +
-		"- Google Docs – atual Google Drive\n" +
-		"- Ferramentas de suporte gráfico Blender 2.62, Photoshop CS5 e Gimp 2.6.12\n" +
-		"- QLite – Banco de Dados específico para Android\n" +
-		"- Trolle – Metodo Kanban online gratuito\n" +
-		"- King Hosting – Hospedagem Compartilhada\n" +
-		"- Google Code Project  Hosting\n\n" +
-		
-		"Linguagens utilizada:\n\n" + 
-		"- Java orientado a objetos\n\n" + 
-		
-		"Referências:\n\n" +
-		"- http://www.google.com\n" +
-		"- http://www.felipesilveira.com.br\n" + 
-		"- http://www.youtube.com\n" + 
-		"- http://www.thecodebakers.org\n" +
-		"- http://developer.android.com/sdk;win-usb.html\n" +
-		"- Livro: Computer graphics for java programmers /  Leen Ammeraal – Chilchester - John Wiley & Sons,  2001.\n" +
-		"- Livro: Google Android - Aprenda a Criar Aplicações para Dispositivos Móveis com o Android SDK - 2ª edição -  Autor: Lecheta, Ricardo R. – Editora: Novatec.\n" +
-		
-		"";
+		return "***Close Box***\n" +
+		"***Versão 1.0***\n\n" +
+		"***2012 The EndGamers***\n\n"+
+		"***Metodologia utilizada:\n\n- SCRUM\n\nDesenvolvedores\n\n-Fernanda Onishi\n-Fernando Gomes\n-Reinaldo Fonseca\n-Rubens Rodrigues\n-Thiago Mascarelo Nascimento\n\n" +
+				"Orientador\n\n- Professor Edson Saraiva\n\n" +
+				
+				"Ferramentas:\n\n"+
+				"- IDE Eclipse versão Helios 3.6 para Java Developers\n" +
+				"- Plugin Android Development Tools (ADT)\n" +
+				"- Java Plataform, Standard Edition Development Kit (JDK) versão mínima 6 update 31\n" +
+				"- Android SDK (Standard Development Kit)\n" +
+				"- Ferramentas de vídeo-conferência e programa que ofereça serviço de Instant Messengers\n" +
+				"- MS Office 2010\n" +
+				"- Google Drive\n" +
+				"- Ferramenta de suporte gráfico Photoshop CS6\n" +
+				"- SQLite – Banco de Dados específico para Android\n" +
+				"- Google Code Project Hosting\n\n" +
+				
+				"Linguagens utilizada:\n\n" + 
+				"- Java Orientado a Objetos\n\n" + 
+				
+				"Referências:\n\n" +
+				"- http://www.felipesilveira.com.br\n" + 
+				"- http://www.thecodebakers.org\n" +
+				"- http://developer.android.com\n" +
+				"- Livro: Computer graphics for java programmers /  Leen Ammeraal – Chilchester - John Wiley & Sons,  2001.\n" +
+				"- Livro: Google Android - Aprenda a Criar Aplicações para Dispositivos Móveis com o Android SDK - 2ª edição -  Autor: Lecheta, Ricardo R. – Editora: Novatec.\n" +
+				"–Livro: Use a Cabeça! - Padrões de Projeto  - 2ª edição revisada – "+
+				"Autores: Freeman E; Freeman E\n\n" +
+
+				"Agradecimentos\n\n"+
+				"Professor Sergio Bonato\n"+
+				"Professora Andréa Zotovici\n"+
+				"Professor Edson Saraiva\n"+
+				"Daniel Souza\n"+
+				"Felipe Nascimento\n"+
+				"Isabela Nascimento\n"+
+				"Alexandre Fonseca\n"+
+				"Julia Fonseca\n"+"";
 	}
 }
